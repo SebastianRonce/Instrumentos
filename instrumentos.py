891eea6bbda0639ca7f01d7b40981d7363609084
SebastianRonce <jronceria@ucundinamarca.edu.co>
2024-09-25 12:40:14 -0500

    Maracas.new

--- instrumentos.py
+++ instrumentos.py
@@ -20,6 +20,10 @@ class Flauta(InstrumentoMusical):
 class Bateria(InstrumentoMusical):
     def tocar(self):
         return f"{self.nombre} está tocando un ritmo de batería."
+    
+class Maracas(InstrumentoMusical):
+    def tocar(self):
+        return f"Se estan agitando {self.nombre}."
 
 def gestionar_instrumentos(instrumentos):
     for instrumento in instrumentos:
@@ -31,7 +35,8 @@ def mostrar_menu():
     print("2. Ver y tocar Piano")
     print("3. Ver y tocar Flauta")
     print("4. Ver y tocar Batería")
-    print("5. Salir")
+    print("5. Ver y tocar Maracas")
+    print("6. Salir")
 
 def main():
     # Crear instancias de instrumentos musicales
@@ -39,19 +44,21 @@ def main():
     piano = Piano("Piano de Cola")
     flauta = Flauta("Flauta Traversera")
     bateria = Bateria("Batería Acústica")
+    maracas = Maracas("Las Maracas")
 
     instrumentos = {
         1: guitarra,
         2: piano,
         3: flauta,
-        4: bateria
+        4: bateria,
+        5: maracas
     }
 
     while True:
         mostrar_menu()
         try:
             opcion = int(input("Seleccione una opción: "))
-            if opcion == 5:
+            if opcion == 6:
                 print("Saliendo del sistema.")
                 break
             elif opcion in instrumentos:
